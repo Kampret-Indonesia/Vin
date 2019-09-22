f59077cfd6a8b1e3dd387987b73c6ffee6c54295
Kampret120 <55634001+Kampret120@users.noreply.github.com>
2019-09-22 08:43:19 +0700

    Create Vin.sh

---
+++ Vin.sh
@@ -0,0 +1,183 @@
+clear
+        echo -n "Masukkan Password Anda : " 
+        read pw; 
+
+        if [ $pw = "Kampret"    ]; 
+        then 
+        echo "Password anda benar" 
+        else 
+        echo "Password anda salah" 
+        echo "Silakan Masukkan kembali" 
+        fi
+        clear
+        bi='\033[34;1m' #biru
+        i='\033[32;1m' #ijo
+        pur='\033[35;1m' #purple
+        cy='\033[36;1m' #cyan
+        me='\033[31;1m' #merah
+        pu='\033[37;1m' #purple
+        ku='\033[33;1m' #kuning
+        Lightred='\033[91m' #Merah Cahaya
+        BrightBlue='\033[34m' #Cahaya Biru
+        echo
+        echo
+        python2 sploit.py
+        echo
+        clear
+        echo
+        echo $blue"          ======================+++++++++++==================="
+        echo $red"                    Sabar ngenthod lagi nginstall..."
+        sleep 1
+        echo '''\a
+        \033[34;1m                              _
+        \033[32;1m                             / |
+        \033[35;1m                             | |
+        \033[36;1m                             | |
+        \033[37;1m                            _|_|_ '''
+        sleep 1
+        apt update && apt upgrade
+        apt install nano
+        apt install git
+        pkg install python python2 vim figlet curl
+        clear
+        echo
+        echo
+        echo
+        echo
+        echo $blue"         ======================+++++++++++==================="
+        echo $red"            Jika Anda Cewek Maka Proses Installasi akan cepat"
+        sleep 1
+        echo '''\a
+        \033[32;1m                             ____
+        \033[34;1m                            |___ \
+        \033[35;1m                             __) |
+        \033[36;1m                            / __/
+        \033[37;1m                           |_____| '''
+        sleep 1
+        apt install php
+        pip2 install mechanize
+        pip2 install lolcat
+        pip2 install requests
+        clear
+        echo
+        echo
+        echo
+        echo
+        echo $blue"         ======================+++++++++++==================="
+        echo $red"            Semakin Gans Semakin Cepat Masuk ke Menu"
+        sleep 1
+        echo '''\a
+        \033[35;1m                            _____
+        \033[36;1m                           |___ /
+        \033[37;1m                             |_ \
+        \033[34;1m                            ___) |
+        \033[32;1m                           |____/ '''
+        sleep 1
+        clear
+        echo
+        echo
+        echo
+        echo
+        echo $blue"         ======================+++++++++++==================="
+        echo $red"            Btw Jika Anda Cewek hubungi :081316577616"
+        sleep 1
+        echo '''\a
+        \033[33;1m                            _  _
+        \033[34;1m                           | || |
+        \033[35;1m                           | || |_
+        \033[36;1m                           |__   _|
+        \033[37;1m                              |_| '''
+        sleep 1
+        clear
+        echo
+        echo
+        echo
+        echo
+        echo $white"         Prosses failed to go to the Main Menu"
+        echo $red"            F For Respect"
+        sleep 1
+        echo '''\a
+        \033[36;1m                             ____
+        \033[35;1m                            | ___|
+        \033[34;1m                            |___ \
+        \033[33;1m                             ___) |
+        \033[32;1m                            |____/ '''
+        sleep 1
+        clear
+        echo
+        echo $pur"==================="$cy" ============================="
+        echo $i"##"$me"  AUTHOR  :  ------Vin------  "$i"              ##"
+        echo $i"##"$pu"  WhatsApp:  085274745487"$i"                  ##"
+        echo $pur"==================="$cy" ============================="
+
+        echo $cy"silahkan pilih tools mana ingin kamu pake"
+        echo
+        echo $cy"-----Vin----- Menu:"
+
+        echo "[===============================================[>"
+        echo $green "1.> Spam sms brutal" "[===============================================]>"
+        echo "[===============================================[>"
+        echo $red "2.>HACK-FB" "[===============================================]>"
+        echo "[===============================================[>"
+        echo $blue "3.>Hack_Cctv" "[===============================================]>"
+        echo "[===============================================[>"
+        echo $white "4.> Ddos_attack" "[===============================================]>"
+        echo $blu "00.> Exit " " [============================[>"
+        echo
+        echo $pur"--Vin--["$pur"Tentukan pilihan kamu sekarang"$p
+        read -p"--Vin--" pil
+
+
+        if [ $pil = 1 ]
+        then
+        clear
+        figlet -f slant "TUNGGU"|lolcat
+        pkg install git
+        git clone https://github.com/TERMUXID3/brutal-sms.git
+        cd brutal-sms
+        python2 run.py
+        fi
+
+
+        if [ $pil = 2 ]
+        then
+        clear
+        figlet -f slant "TUNGGU"|lolcat
+        sleep 1
+        Apt update && apt upgrade
+        Apt install git
+        pkg install python2
+        git clone https://github.com/MARA345/HACK-FB
+        pkg install machanize
+        cd HACK-FB
+        python2 MARA210enc.py
+fi
+
+
+        if [ $pil = 3 ]                                                                                    
+        then
+        clear
+        figlet -f slant "TUNGGU"|lolcat
+        sleep 1
+        Apt update && apt upgrade
+        Apt install git
+        pkg install python2
+        git clone https://github.com/kancotdiq/ipcs
+        cd ipcs
+        python2 scan.py
+        fi
+
+
+        if [ $pil = 4 ]
+        then
+        clear
+        figlet -f slant "TUNGGU"|lolcat
+        sleep 1
+        Apt update && apt upgrade
+        Apt install git
+        pkg install python
+        pip2 install request
+        git clone https://github.com/cyweb/hammer
+        cd hammer
+        python hammer.py
+        fi
